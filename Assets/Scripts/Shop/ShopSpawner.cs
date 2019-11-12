b57276f7931deeb5357ca275e4114f9bd3eead82
SanteriSuomi <sant.suomi@gmail.com>
2019-11-12 13:26:51 +0200

    wip shop menu

---
+++ Assets/Scripts/Shop/ShopSpawner.cs
@@ -0,0 +1,13 @@
+﻿using Shooter.Utility;
+using UnityEngine;
+
+namespace Shooter.UI
+{
+    public class ShopSpawner : Singleton<ShopSpawner>
+    {
+        public void Spawn(GameObject gameObject)
+        {
+            // To-do?
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GameUICanvasManager.cs
@@ -0,0 +1,16 @@
+﻿using Shooter.Utility;
+using UnityEngine;
+
+namespace Shooter.UI
+{
+    public class GameUICanvasManager : Singleton<GameUICanvasManager>
+    {
+        [SerializeField]
+        private GameObject followerShopButton = default;
+
+        public void FollowerShopButton()
+        {
+            followerShopButton.SetActive(!gameObject.activeSelf);
+        }
+    }
+}
